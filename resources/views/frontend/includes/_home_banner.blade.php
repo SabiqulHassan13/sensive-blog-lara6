bc297c393646af13e85d6c04812b94b880362910
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2020-06-25 22:24:18 +0600

    DB index length fixed

--- resources/views/frontend/includes/_home_banner.blade.php
+++ resources/views/frontend/includes/_home_banner.blade.php
@@ -1,6 +1,6 @@
 <section class="banner-area">
 	<div class="container box_1170">
-		<div class="row fullscreen d-flex align-items-center justify-content-center">
+		<div class="row fullscreen d-flex align-items-center justify-content-center" style="height: 347px;">
 			<div class="banner-content text-center col-lg-8">
 				<h1>
 					Charter Yacht <br>
