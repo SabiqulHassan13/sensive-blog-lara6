40a824acaf47f05746e1e119dbd5403751f02304
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2020-06-26 01:11:37 +0600

    database migration files created

---
+++ database/migrations/2020_06_25_162655_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('body');
+            $table->string('image');
+            $table->boolean('status');
+            $table->integer('posted_by');
+            $table->integer('likes');
+            $table->integer('dislikes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
