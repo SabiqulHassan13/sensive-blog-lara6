2d86b06a25e1cefa553d3119a3434b88f87b3e77
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2020-06-26 01:18:10 +0600

    Home button set in admin panel for frontend home page link

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -27,7 +27,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="../../index3.html" class="nav-link">Home</a>
+        <a href="{{ route('home') }}" target="_blank" class="nav-link btn btn-secondary text-white">Home</a>
       </li>
 
     </ul>
@@ -185,7 +185,7 @@
            alt="AdminLTE Logo"
            class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Admin Panel</span>
     </a>
 
     <!-- Sidebar -->
